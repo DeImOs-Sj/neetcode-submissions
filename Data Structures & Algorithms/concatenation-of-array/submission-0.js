8f31fef8b2872f2d87141a13c5279b0b18577223
Shlok Jagtap <115064345+DeImOs-Sj@users.noreply.github.com>
2026-08-14 17:52:25 +0530

    Add: concatenation-of-array - submission-0

---
+++ Data Structures & Algorithms/concatenation-of-array/submission-0.js
@@ -0,0 +1,16 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number[]}
+     */
+    getConcatenation(nums) {
+    let answer = []
+    let i=0
+
+    answer.push(...nums)
+    answer.push(...nums)
+
+
+return answer
+    }
+}
