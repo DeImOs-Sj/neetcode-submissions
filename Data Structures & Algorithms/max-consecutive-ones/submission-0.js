75dff5f8a4af37c92125fb87bd43ac326fffb4dd
Shlok Jagtap <115064345+DeImOs-Sj@users.noreply.github.com>
2026-08-22 23:58:33 +0530

    Add: max-consecutive-ones - submission-0

---
+++ Data Structures & Algorithms/max-consecutive-ones/submission-0.js
@@ -0,0 +1,24 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number}
+     */
+    findMaxConsecutiveOnes(nums)  {
+    let count=0
+    let maxCount=0
+    for(let i = 0 ;i<nums.length;i++){
+        if(nums[i]===0){
+            count=0
+        }
+        else{
+            count = count +1 
+            if(count > maxCount){
+                maxCount = count
+            }
+
+        }
+    }
+    return maxCount
+    
+}
+}
