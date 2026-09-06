d0596f334459a9f3cd41c7934395572cc8bae555
Shlok Jagtap <115064345+DeImOs-Sj@users.noreply.github.com>
2026-09-06 21:03:10 +0530

    Add: minimum-stack - submission-1

---
+++ Data Structures & Algorithms/minimum-stack/submission-1.ts
@@ -0,0 +1,47 @@
+class MinStack {
+    stack:number[] = []
+    minStack:number[] = []
+    constructor() {
+        this.stack = []
+        this.minStack = []
+    }
+
+    /**
+     * @param {number} val
+     * @return {void}
+     */
+    push(val: number): void {
+        this.stack.push(val)
+
+        if(this.minStack.length === 0 || val<=this.minStack[this.minStack.length-1] ){
+            this.minStack.push(val)
+        }
+
+    
+    }
+
+    /**
+     * @return {void}
+     */
+    pop(): void {
+        let value=this.stack.pop()
+        if(value===this.minStack[this.minStack.length-1]){
+            this.minStack.pop()
+        }
+    }
+
+    /**
+     * @return {number}
+     */
+    top(): number {
+        return this.stack[this.stack.length - 1]
+    }
+
+    /**
+     * @return {number}
+     */
+    getMin(): number {
+        return this.minStack[this.minStack.length-1]
+
+    }
+}
