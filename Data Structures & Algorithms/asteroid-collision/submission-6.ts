b2e07cc8586aa3eb63b690ffe6fe76ea024b601d
Shlok Jagtap <115064345+DeImOs-Sj@users.noreply.github.com>
2026-09-07 01:14:13 +0530

    Add: asteroid-collision - submission-6

---
+++ Data Structures & Algorithms/asteroid-collision/submission-6.ts
@@ -0,0 +1,33 @@
+class Solution {
+
+    asteroidCollision(asteroids: number[]): number[] {
+        let stack = []
+        let i = 0
+        while(i<asteroids.length){
+            while(stack.length != 0 && stack[stack.length-1]>0 && asteroids[i] <0  ){
+                if(Math.abs(asteroids[i])>Math.abs(stack[stack.length-1])){
+                    stack.pop()
+                    continue
+                }
+                else if(Math.abs(stack[stack.length-1])===Math.abs(asteroids[i])){
+                    stack.pop()
+                    i++
+                    continue
+                }
+                else{
+                    i++
+                    continue
+                }
+            }
+  if (i < asteroids.length) {
+                stack.push(asteroids[i])
+            }            i++
+            
+
+        }
+        return stack
+
+
+    }
+
+}
