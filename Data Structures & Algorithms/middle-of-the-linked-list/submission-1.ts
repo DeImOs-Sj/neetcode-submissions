52f7cbfa564f8c33d26f4f91ac53aeaee31229e3
Shlok Jagtap <115064345+DeImOs-Sj@users.noreply.github.com>
2026-09-13 20:15:35 +0530

    Add: middle-of-the-linked-list - submission-1

---
+++ Data Structures & Algorithms/middle-of-the-linked-list/submission-1.ts
@@ -0,0 +1,28 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     constructor(val = 0, next = null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+class Solution {
+    /**
+     * @param {ListNode} head
+     * @return {ListNode}
+     */
+    middleNode(head: ListNode | null): ListNode {
+        let slow = head;
+        let fast = head;
+        while (fast.next != null && fast.next.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        if (fast.next != null) {
+            return slow.next;
+        } else {
+            return slow;
+        }
+    }
+}
